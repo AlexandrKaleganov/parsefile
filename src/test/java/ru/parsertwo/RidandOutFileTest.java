9478359c8ae42f95a6bf101fc76a69d0b7ca3ddc
AlexandrKaleganov <alexmur07@mail.ru>
2018-08-25 02:38:12 +0700

    Не запускается тестирование проекта

--- src/test/java/ru/parsertwo/RidandOutFileTest.java
+++ src/test/java/ru/parsertwo/RidandOutFileTest.java
@@ -19,6 +19,5 @@ public class RidandOutFileTest {
         RidandOutFile ridandOutFile = new RidandOutFile();
         Path path = get("file//google.csv");
         ridandOutFile.send(path.toString());
-
     }
 }

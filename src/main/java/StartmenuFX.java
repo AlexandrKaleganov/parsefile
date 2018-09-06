6e2306c474cc442020f63bab13c1d1a451f73007
iSmokeJC <ismoke@yandex.ru>
2018-09-06 18:51:04 +0300

    поправил помник, дальше смотри, что там с пакетами делать, засада там. так работает

--- src/main/java/StartmenuFX.java
+++ src/main/java/StartmenuFX.java
@@ -1,14 +1,10 @@
-package ru.parsertwo;
-
 import javafx.application.Application;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-
-
-public class StartmenuFX extends Application{
+public class StartmenuFX extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         AnchorPane panel = FXMLLoader.load(getClass().getResource("startmenuFX.fxml"));
@@ -16,10 +12,10 @@ public class StartmenuFX extends Application{
         primaryStage.setTitle("test");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
